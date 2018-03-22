2b7a4ae1c83f6bc79d2fc136546e29753542ae9b
Cássio Zen <cassiozen@gmail.com>
2018-03-22 16:13:30 -0400

    Startng point

--- client/components/App.js
+++ client/components/App.js
@@ -4,13 +4,14 @@ import MouseTracker from "./MouseTracker";
 class App extends Component {
   render() {
     return (
-      <MouseTracker render={({x, y}) => <img 
-          src="http://pngimg.com/uploads/cat/cat_PNG132.png"
-          width="100"
-          style={{position: "absolute", top: y-22, left:x-10}}
-        />}
+      <MouseTracker
+        render={({ x, y }) => (
+          <p>
+            The current mouse position is ({x}, {y})
+          </p>
+        )}
       />
-    )
+    );
   }
 }
 
