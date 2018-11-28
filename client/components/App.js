652b2feab885eb27367093e48c3d0788c76ebf1e
Wenyi Zheng <hilarywenyi@gmail.com>
2018-11-28 16:08:37 -0500

    added picture in mouse and mouse is using higher-order component in React

--- client/components/App.js
+++ client/components/App.js
@@ -5,11 +5,16 @@ class App extends Component {
   render() {
     return (
       <MouseTracker
-        render={({ x, y }) => (
-          <p>
-            The current mouse position is ({x}, {y})
-          </p>
-        )}
+        // render={({ x, y }) => (
+        //   <p>
+        //     The current mouse position is ({x}, {y})
+        //   </p>
+        // )}
+        render={({x, y}) => <img 
+          src="http://pngimg.com/uploads/cat/cat_PNG132.png"
+          width="80"
+          style={{position: "absolute", top: y-20, left:x-5}}
+        />}
       />
     );
   }
--- client/components/MouseTracker.js
+++ client/components/MouseTracker.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 
 class MouseTracker extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+       x: 0,
+       y: 0
+    }
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  handleMouseMove(event){
+    const x = event.clientX;
+    const y = event.clientY;
+    this.setState({x,y})
+  }
   render() {
-    return <div className="mouse-tracker">RENDER PROP HERE</div>;
+    return <div className="mouse-tracker" onMouseMove ={this.handleMouseMove}>{this.props.render(this.state)}</div>;
   }
 }
 
